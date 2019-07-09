f2060f5ad33ff9e43ec5c121ba79f770120fc18b
BreddySome <49569395+BreddySome@users.noreply.github.com>
2019-07-09 14:32:07 +0600

    Add files via upload

---
+++ sound/npc/sh_sound.lua
@@ -0,0 +1,10 @@
+sound.Add( {
+	name = "die1",
+	channel = CHAN_STATIC,
+	volume = 10,
+	level = 80,
+	pitch = { 95, 110 },
+	sound = "npc/metropolice/die1.wav"
+} )
+
+sound.AddSoundOverrides( "npc/metropolice/die1.wav" )
